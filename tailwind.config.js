bca581cabe6270635ec6f63ea6b6c4b2f245ad48
Minister <mundestephane13@gmail.com>
2023-06-19 23:09:03 +0100

    Created page to me edited by Minister

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: royalblue
+      }
+    },
   },
   plugins: [],
 }
